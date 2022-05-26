b846ce49ec6e70a657bd5289336bbe2e18689e85
Nicole Caceres <janenieggcs@gmail.com>
2022-05-26 17:26:01 -0500

    Cuarto Commit - Create Funcionando

--- src/main/java/com/tecsup/petclinic/services/OwnerServiceImpl.java
+++ src/main/java/com/tecsup/petclinic/services/OwnerServiceImpl.java
@@ -1,10 +1,11 @@
 package com.tecsup.petclinic.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.tecsup.petclinic.entities.Owner;
 import com.tecsup.petclinic.repositories.OwnerRepository;
-
+@Service
 public class OwnerServiceImpl implements OwnerService{
 	
 
